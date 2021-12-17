d3b8ed026f2956172edb833bc4133caa051272b1
Girish Joshi <girish@opencraft.com>
2021-12-17 17:17:56 +0530

    Added openedx.core.lib.blockstore_router for the Blockstore database
    Signed-off-by: Girish Joshi <girish@opencraft.com>

---
+++ openedx/core/lib/blockstore_router.py
@@ -0,0 +1,96 @@
+"""
+Database Routers for use with the blockstore django app.
+"""
+
+
+class BlockstoreRouter:
+    """
+    A Database Router that separates Blockstore into its own database.
+    """
+
+    DATABASE_NAME = 'blockstore_db'
+    model_types_for_blockstore = ['Collection', 'Bundle', 'BundleVersion', 'Draft', 'BundleLink']
+
+    def _is_csm(self, model):
+        """
+        Return True if ``model`` is Blockstore.apps.bundles.Bundle
+        """
+        
+        return (
+            model._meta.app_label == 'blockstore' and
+            (type(model).__name__ in model_types_for_blockstore
+        )
+
+    def _is_csm_h(self, model):
+        """
+        Return True if ``model`` is Blockstore.apps.bundles.Bundle.
+        """
+        return (
+            model._meta.app_label == 'blockstore' and
+            (
+                type(model).__name__ == 'blockstore' or
+                getattr(model, '__name__', '') in model_types_for_blockstore
+            )
+        )
+
+    def db_for_read(self, model, **hints):  # pylint: disable=unused-argument
+        """
+        Use the BlockstoreRouter.DATABASE_NAME if the model is one of following 
+        - Collection
+        - Bundle
+        - BundleVersion
+        - Draft
+        - BundleLink
+        """
+        if self._is_csm_h(model):
+            return self.DATABASE_NAME
+        else:
+            return None
+
+    def db_for_write(self, model, **hints):  # pylint: disable=unused-argument
+        """
+        Use the BlockstoreRouter.DATABASE_NAME if the model is one of following 
+        - Collection
+        - Bundle
+        - BundleVersion
+        - Draft
+        - BundleLink
+        """
+        if self._is_csm_h(model):
+            return self.DATABASE_NAME
+        else:
+            return None
+
+    def allow_relation(self, obj1, obj2, **hints):  # pylint: disable=unused-argument
+        """
+        Manage relations if the model is one of following 
+        - Collection
+        - Bundle
+        - BundleVersion
+        - Draft
+        - BundleLink
+        """
+        # Allow relation between CSM and CSMH (this cross-database relationship is declared with db_constraint=False
+        # so while cross-model relationship is allowed via Django it is not stored as such within the database).
+        # Note: The order of obj1 and obj2 are based on the parent-child relationship as explained in
+        #   https://github.com/django/django/blob/stable/2.2.x/django/db/models/fields/related_descriptors.py
+        if self._is_csm(obj1) and self._is_csm_h(obj2):
+            return True
+
+        # Prevent any other relations with CSMH since CSMH is in its own different database.
+        elif self._is_csm_h(obj1) or self._is_csm_h(obj2):
+            return False
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):  # pylint: disable=unused-argument
+        """
+        Only sync Blockstore to BlockstoreRouter.DATABASE_Name
+        """
+        if model_name is not None:
+            model = hints.get('model')
+            if model is not None and self._is_csm_h(model):
+                return db == self.DATABASE_NAME
+        if db == self.DATABASE_NAME:
+            return False
+
+        return None
